dc7599bc5d3be43cbfdc1f1935cc15e05554aaa9
dmg <119745411+Jengdapa@users.noreply.github.com>
2023-05-29 06:48:04 +0700

    Update utils.py

--- plugins/utils.py
+++ plugins/utils.py
@@ -14,6 +14,7 @@ from config import (
     FORCE_MSG,
     FORCE_SUB_CHANNEL,
     FORCE_SUB_GROUP,
+    FORCE_SUB_GROUP2,
     HEROKU_API_KEY,
     HEROKU_APP_NAME,
     LOGGER,
@@ -58,6 +59,7 @@ ADMINS = <code>{ADMINS}</code>
 CHANNEL_ID = <code>{CHANNEL_ID}</code>
 FORCE_SUB_CHANNEL = <code>{FORCE_SUB_CHANNEL}</code>
 FORCE_SUB_GROUP = <code>{FORCE_SUB_GROUP}</code>
+FORCE_SUB_GROUP2 = <code>{FORCE_SUB_GROUP2}</code>
 PROTECT_CONTENT = <code>{PROTECT_CONTENT}</code>
 START_MSG = <code>{START_MSG}</code>
 FORCE_MSG = <code>{FORCE_MSG}</code>
